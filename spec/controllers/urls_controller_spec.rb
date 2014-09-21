4bcd48a6afc21f3792af2ed74814c72eb7d5a227
Puru Dahal <pdahal@me.com>
2014-09-20 19:41:14 -0700

    Writes test to return recent 20 urls as an json object

--- spec/controllers/urls_controller_spec.rb
+++ spec/controllers/urls_controller_spec.rb
@@ -1,11 +1,20 @@
 require 'rails_helper'
 
 describe UrlsController do
-  let!(:all_urls) {20.times{Url.create(long_url: Faker::Internet.url)}}
+  let!(:urls) {100.times{Url.create(long_url: Faker::Internet.url)}}
   context "#index" do
     it "renders the application.html.erb layout and index template" do
       expect(get :index).to render_template(layout: 'application')
       expect(get :index).to render_template :index
     end
   end
+
+  context "#recent_links" do
+    it "return 20 urls as json object" do
+      get :recent_links
+      jsonify = JSON.parse(response.body)
+      expect(jsonify.count).to eq(20)
+    end
+  end
+
 end
