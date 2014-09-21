a7b51432f9fa093a4efd54135b7244cdd730f616
Puru Dahal <pdahal@me.com>
2014-09-20 18:36:45 -0700

    Writes test to validate the uniqueness of short url

--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -14,7 +14,10 @@ describe Url do
     it "Validates the given long url to be invalid" do
       expect(invalid_url.valid?).to be(false)
     end
-    it "Validates the uniqueness of short url"
+    it "Validates the uniqueness of short url" do
+      new_url = Url.create(long_url: 'Awesomeurl.com/long')
+      expect(new_url).to validate_uniqueness_of(:short_url)  
+    end
     it "Validates that it shortens the url before save" do
       expect(valid_url).to receive(:shortify)
       valid_url.shortify
