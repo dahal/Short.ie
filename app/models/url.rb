bfe55d8e35afc33715f6bba7d2906debde260f59
Puru Dahal <pdahal@me.com>
2014-09-20 17:44:06 -0700

    Writes the activerecord validation to validate the format of long_url with regex

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,3 +1,7 @@
 class Url < ActiveRecord::Base
   validates :long_url, presence: true
+  validates_format_of :long_url,
+  with: /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/,
+  multiline: true,
+  on: :create
 end
--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe Url do
   context "Validation" do
     let(:valid_url) { FactoryGirl.create(:url)  } 
-    let(:invalid_url) { Url.create(long_url: 'google.com') }
+    let(:invalid_url) { Url.create(long_url: 'google') }
 
     it "Validates the presence of long url" do
       expect(valid_url).to validate_presence_of(:long_url)
