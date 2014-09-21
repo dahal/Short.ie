55f2fd522e353aee2fe1c3be6868687071bb57a9
Puru Dahal <pdahal@me.com>
2014-09-20 19:04:59 -0700

    Writes urls_controller#index test to render application layout template

--- spec/controllers/urls_controller_spec.rb
+++ spec/controllers/urls_controller_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 describe UrlsController do
   context "#index" do
-    it "renders the application.html.erb layout"
+    it "renders the application.html.erb layout" do
+      expect(get :index).to render_template(layout: 'application')
     end
     it "return all domains as an json object"
   end
