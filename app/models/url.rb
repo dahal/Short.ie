f89417ae00deb5ffba2977d8d0cab1f217235010
Puru Dahal <pdahal@me.com>
2014-09-20 18:34:57 -0700

    Long url now gets shortened before being saved

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,7 +1,12 @@
+require "securerandom"
 class Url < ActiveRecord::Base
   validates :long_url, presence: true
   validates_format_of :long_url,
   with: /^((ftp|http|https):\/\/)?([a-zA-Z0-9]+(\.[a-zA-Z0-9]+)+.*)$/,
   multiline: true,
   on: :create
+
+  def shortify
+    self.short_url ||= SecureRandom.base64(3)
+  end
 end
